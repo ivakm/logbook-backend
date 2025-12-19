b407a28f68aacee9d8e7be1e7fcc246cfc941b44
Shevchenko Dmytro <dimashevchenko594@gmail.com>
2025-12-19 23:36:25 +0200

    Initial Spring Boot setup

---
+++ src/test/java/com/example/logbookback/LogbookBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logbookback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbookBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
